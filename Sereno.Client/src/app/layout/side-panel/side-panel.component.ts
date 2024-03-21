02abdc1cdc8797aa72e71c049572a6e4e343008e
GerhardAllgaeuer <gerhard.allgaeuer@live.com>
2024-03-21 18:13:01 +0100

    feat: Icon Change

--- Sereno.Client/src/app/layout/side-panel/side-panel.component.ts
+++ Sereno.Client/src/app/layout/side-panel/side-panel.component.ts
@@ -3,30 +3,30 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 export interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
 }
 
 export const ROUTES: RouteInfo[] = [
-    { path: '/contacts',      title: 'Kontakte',          icon:'nc-bank',       class: '' },
-    { path: '/inbox',         title: 'Eingang',           icon:'nc-diamond',    class: '' },
+  { path: '/contacts', title: 'Kontakte', icon: 'icon-side-incoming', class: '' },
+  { path: '/inbox', title: 'Eingang', icon: 'icon-side-new', class: '' },
 ];
 
 @Component({
-    moduleId: module.id,
-    imports: [
-        RouterModule, CommonModule
-      ],
-        standalone: true,
-    selector: 'sidebar-cmp',
-    templateUrl: 'side-panel.component.html',
+  moduleId: module.id,
+  imports: [
+    RouterModule, CommonModule
+  ],
+  standalone: true,
+  selector: 'sidebar-cmp',
+  templateUrl: 'side-panel.component.html',
 })
 
 export class SidePanelComponent implements OnInit {
-    public menuItems: any[];
-    ngOnInit() {
-        this.menuItems = ROUTES.filter(menuItem => menuItem);
-    }
+  public menuItems: any[];
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
 }
